c96f3205e157b989e972030ce705bb30b8735487
zhaoyan8 <zhaoyan8@xiaomi.com>
2023-05-30 22:03:53 -0500

    first commit

---
+++ src/Prog3.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prog3 {
+    public static void main(String[] args){
+        String[] str = new String[]{"horse", "dog", "cat", "horse", "dog"};
+        List<String> list = new ArrayList<String>();
+
+        for(int i=0; i<str.length; i++){
+            if(!list.contains(str[i])){
+                list.add(str[i]);
+            }
+        }
+
+        System.out.println(list);
+    }
+}
